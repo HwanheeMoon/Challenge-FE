8b84da7c40db317413f8470951d652165ce4da74
HwanheeMoon <yuiop0218@naver.com>
2024-12-12 11:12:23 +0900

    :tada: Fix: tailwind DI modify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 export const content = ["./src/**/*.{html,js,ts,tsx,jsx}"];
 export const theme = {
-  extend: {
-    // borderColor : "#ff6f70"
-  },
+   extend: {
+      // borderColor : "#ff6f70"
+   },
 };
-export const plugins = [require("daisyui")];
+export const plugins = require("daisyui");
