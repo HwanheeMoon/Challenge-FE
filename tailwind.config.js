df2d1bd3bf6abb4ac9f7fcf3f8d1653e6e20ccc2
HwanheeMoon <yuiop0218@naver.com>
2024-12-12 13:08:30 +0900

    :ambulance: !Hotfix : 테일윈드 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
-export const content = ["./src/**/*.{html,js,ts,tsx,jsx}"];
-export const theme = {
-   extend: {
-      // borderColor : "#ff6f70"
+/** @type {import {  } from "(tailwindcss).Config";} */
+module.exports = {
+   content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+   theme: {
+      extend: {},
    },
+   plugins: [require("daisyui")],
 };
-export const plugins = require("daisyui");
+
+// export const content = ["./src/**/*.{html,js,ts,tsx,jsx}"];
+// export const theme = {
+//    extend: {
+//    },
+// };
+// export const plugins = [require("daisyui")];
